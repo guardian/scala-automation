a18b1ed81814d419fc1a89377b46c36c13e21b31
John Duffell <john.duffell@guardian.co.uk>
2014-06-10 12:01:51 +0100

    add test for user config getter

--- src/test/scala/com/gu/support/ConfigTest.scala
+++ src/test/scala/com/gu/support/ConfigTest.scala
@@ -8,12 +8,12 @@ import java.io.InputStreamReader
  */
 class ConfigTest extends FlatSpec with Matchers {
 
-  "The Config Loader" should "get a default value from the framework.conf without a local file" in {
+  "The Config" should "get a default value from the framework.conf without a local file" in {
     val configLoader = new Config(None, getReader("project1.conf"), getReader("framework1.conf"))
     configLoader.getWebDriverRemoteUrl() should be ("")
   }
 
-  "The Config Loader" should "get a default value from the framework.conf with a local file" in {
+  "The Config" should "get a default value from the framework.conf with a local file" in {
     val configLoader = new Config(Some(getReader("local1.conf")), getReader("project1.conf"), getReader("framework1.conf"))
     configLoader.getWebDriverRemoteUrl() should be ("")
   }
@@ -26,6 +26,11 @@ class ConfigTest extends FlatSpec with Matchers {
   it should "get a local machine specific value from local.conf" in {
     val configLoader = new Config(Some(getReader("local2.conf")), getReader("project1.conf"), getReader("framework1.conf"))
     configLoader.getTestBaseUrl() should be ("http://www.localtest.com")
+  }
+
+  "The Config" should "be able to supply user config items" in {
+    val configLoader = new Config(Some(getReader("localUserConfig.conf")), getReader("project1.conf"), getReader("framework1.conf"))
+    configLoader.getUserValue("cheese") should be ("hello")
   }
 
   // helper method
