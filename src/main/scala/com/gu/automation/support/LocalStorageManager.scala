5549327e6e31947dd20dc5d221975b219694ab18
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-03-23 15:17:54 +0000

    Strip quotes from key/value input and use double quotes in JS

--- src/main/scala/com/gu/automation/support/LocalStorageManager.scala
+++ src/main/scala/com/gu/automation/support/LocalStorageManager.scala
@@ -6,16 +6,20 @@ import scala.util.Try
 
 object LocalStorageManager {
 
+  private def stripQuotes(input: String): String = {
+    return input filterNot ("'\"" contains _)
+  }
+
   private def executeJavascript(javascript: String)(implicit driver: WebDriver): Try[String] = {
     return Try(driver.asInstanceOf[JavascriptExecutor].executeScript(javascript).toString)
   }
 
   def remove(key: String)(implicit driver: WebDriver) = {
-    executeJavascript(s"localStorage.removeItem('$key');")
+    executeJavascript("localStorage.removeItem(\"%s\");" format (stripQuotes(key)))
   }
 
   def set(key: String, value: String)(implicit driver: WebDriver) = {
-    executeJavascript(s"localStorage.setItem('$key', '$value');")
+    executeJavascript("localStorage.setItem(\"%s\", \"%s\");" format (stripQuotes(key), stripQuotes(value)))
   }
 
 }
